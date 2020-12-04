af098d90c556079b675817f53b173a93adf02cc5
Yuuki8568 <73991053+Yuuki8568@users.noreply.github.com>
2020-12-04 16:23:26 +0900

    ユーザー管理機能　新規登録　DB登録まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+    private
+
+    def user_params
+      params.require(:user).permit(:nickname, :first_name, :first_name_kana, :last_name, :last_name_kana, :birth_day).merge(user_id: current_user.id)
+    end
 end
